455e461de829112a500ceacbea599be1921d05cf
Phaneendra Mettu <mettu.phaneendra@gmail.com>
2018-12-15 12:10:37 -0800

    parse station info for each site: name, has_media_issues

--- site_data_viz.py
+++ site_data_viz.py
@@ -20,17 +20,24 @@ def get_volta_data(url):
         if 'name' in site and not (site['name'] is None):
             _name = site['name']
             print ("Site # {}, name {}:".format(_count, _name))
+
         if 'location' in site and not (site['location'] is None):
             _latitude = site['location']['coordinates'][0]
             _longitude = site['location']['coordinates'][1]
             print ("Location :({}, {})".format(_latitude, _longitude))
+
         if 'chargers' in site and not (site['chargers'] is None):
             _total_chargers = (site['chargers'][0]['total'])
             _available_chargers = (site['chargers'][0]['available'])
             print ("Total chargers :{}; Available chargers :{}".format(_total_chargers, _available_chargers))
         else:
             print ("Total chargers :{}; Available chargers :{}".format(0, 0))
-            
+
+        for count in range(len(site['stations'])):
+            _station = site['stations'][count]['name']
+            _media = str(site['stations'][count]['has_media_issue'])
+            print ("\tStation :{}; has media issues :{}".format(_station, _media))
+
 def main():
     site_metrics_url = 'https://api.voltaapi.com/v1/sites-metrics'
     get_volta_data(site_metrics_url);
