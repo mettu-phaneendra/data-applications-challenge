424ca084896d84a5692cb76332de2e89a41cb0b7
Phaneendra Mettu <mettu.phaneendra@gmail.com>
2018-12-15 11:50:58 -0800

    parse basic info for each site, lat, log and name

--- site_data_viz.py
+++ site_data_viz.py
@@ -10,6 +10,20 @@ def get_volta_data(url):
     d_metrics['size'] = len(site_metrics)
     print "No. of sites :", d_metrics['size']
 
+    # build the site metrics data from resulted json
+    _count = 1
+    for site in site_metrics:
+        _name = ''
+        _latitude = 0
+        _longitude = 0
+        if 'name' in site and not (site['name'] is None):
+            _name = site['name']
+            print ("Site # {}, name {}:".format(_count, _name))
+            _count = _count + 1;
+        if 'location' in site and not (site['location'] is None):
+            _latitude = site['location']['coordinates'][0]
+            _longitude = site['location']['coordinates'][1]
+            print ("Location :({}, {})".format(_latitude, _longitude))
 
 def main():
     site_metrics_url = 'https://api.voltaapi.com/v1/sites-metrics'
